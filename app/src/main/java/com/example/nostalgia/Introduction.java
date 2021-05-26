530a9e2d411bd98ef7d284844fe6b7427ff3cc67
Aravind Varma <dl.aravind@gmail.com>
2021-05-26 10:23:26 +0200

    Introduction will only occur once. Used SharedPreferences for implementation

--- app/src/main/java/com/example/nostalgia/Introduction.java
+++ app/src/main/java/com/example/nostalgia/Introduction.java
@@ -23,56 +23,72 @@ public class Introduction extends AppCompatActivity {
 
     private EditText mUsername;
     private Button mContinue;
+    public static final String FIRST_TIME = "firsttime";
     public static final String SEND_USERNAME= "username";
     public static final String APPLICABLE_EVENTS = "true_events";
-    public static String globalPreference = "com.exampl.nostalgia";
+    public static String globalPreference = "com.example.nostalgia";
     private RadioButton mUserYesStudent, mUserYesWorked,mUserYesReligious;
     private List<String> allEvents = new LinkedList<String>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.userdetails);
-        allEvents.add("Student Life");allEvents.add("Work");allEvents.add("Festivals");allEvents.add("Home");
-        allEvents.add("Birthdays");allEvents.add("Hangouts");
-        mUsername = (EditText) findViewById(R.id.user_name);
-        mUsername.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if (sp.getBoolean(FIRST_TIME, false)) {
+            Intent intent = MemoryListActivity.newIntent(getApplicationContext());
+            startActivity(intent);
+        } else {
 
-        mUserYesStudent = (RadioButton) findViewById(R.id.yes_student);
-        mUserYesWorked = (RadioButton) findViewById(R.id.yes_worked);
-        mUserYesReligious = (RadioButton) findViewById(R.id.yes_religious);
-        mContinue = (Button) findViewById(R.id.continue_button);
-        mContinue.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if(!mUserYesStudent.isChecked())
-                    allEvents.remove("Student Life");
-                if(!mUserYesWorked.isChecked())
-                    allEvents.remove("Work");
-                if(!mUserYesReligious.isChecked())
-                    allEvents.remove("Festivals");
+            setContentView(R.layout.userdetails);
+            allEvents.add("Student Life");
+            allEvents.add("Work");
+            allEvents.add("Festivals");
+            allEvents.add("Home");
+            allEvents.add("Birthdays");
+            allEvents.add("Hangouts");
+            mUsername = (EditText) findViewById(R.id.user_name);
+            mUsername.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
 
-                String[] applicableEvents = {};applicableEvents = allEvents.toArray(applicableEvents);
-                StringBuilder combinedEvents = new StringBuilder();
-                for (int i = 0; i<applicableEvents.length; i++)
-                    combinedEvents.append(applicableEvents[i]).append(",");
-                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-                editor.putString(SEND_USERNAME, mUsername.getText().toString());
-                editor.putString(APPLICABLE_EVENTS, combinedEvents.toString());
-                editor.apply();
-                Intent intent = MemoryListActivity.newIntent(getApplicationContext());
-                startActivity(intent);
-            }
-        });
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+
+            mUserYesStudent = (RadioButton) findViewById(R.id.yes_student);
+            mUserYesWorked = (RadioButton) findViewById(R.id.yes_worked);
+            mUserYesReligious = (RadioButton) findViewById(R.id.yes_religious);
+            mContinue = (Button) findViewById(R.id.continue_button);
+            mContinue.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!mUserYesStudent.isChecked())
+                        allEvents.remove("Student Life");
+                    if (!mUserYesWorked.isChecked())
+                        allEvents.remove("Work");
+                    if (!mUserYesReligious.isChecked())
+                        allEvents.remove("Festivals");
+
+                    String[] applicableEvents = {};
+                    applicableEvents = allEvents.toArray(applicableEvents);
+                    StringBuilder combinedEvents = new StringBuilder();
+                    for (int i = 0; i < applicableEvents.length; i++)
+                        combinedEvents.append(applicableEvents[i]).append(",");
+                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+                    editor.putBoolean(FIRST_TIME,true);
+                    editor.putString(SEND_USERNAME, mUsername.getText().toString());
+                    editor.putString(APPLICABLE_EVENTS, combinedEvents.toString());
+                    editor.apply();
+                    Intent intent = MemoryListActivity.newIntent(getApplicationContext());
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
