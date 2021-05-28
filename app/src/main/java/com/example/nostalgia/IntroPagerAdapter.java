2f085376d06e0ae6015789d1b06be88bc1267554
Aravind Varma <dl.aravind@gmail.com>
2021-05-29 00:08:43 +0200

    Introduction viewpage and logic complete. Add little dots at bottom and improve UX

--- app/src/main/java/com/example/nostalgia/IntroPagerAdapter.java
+++ app/src/main/java/com/example/nostalgia/IntroPagerAdapter.java
@@ -1,22 +1,43 @@
 package com.example.nostalgia;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.example.nostalgia.Introduction.APPLICABLE_EVENTS;
+import static com.example.nostalgia.Introduction.FIRST_TIME;
+import static com.example.nostalgia.Introduction.SEND_USERNAME;
+
 public class IntroPagerAdapter extends PagerAdapter {
 
     Context mContext;
-    Activity mActivity;
-
-    public IntroPagerAdapter(Context context,Activity activity){
+    private List<String> allEvents = new LinkedList<String>();
+    private EditText mUsername;
+    private RadioButton mUserYesStudent,mUserYesWorked, mUserYesReligion;
+    public IntroPagerAdapter(Context context){
         this.mContext = context;
-        this.mActivity = activity;
     }
 
     @NonNull
@@ -24,43 +45,104 @@ public class IntroPagerAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View v = inflater.inflate(R.layout.introduction,container,false);
-        int resID = 0;
+
         switch(position){
             case 0:
-                resID = R.id.welcome;
-                LinearLayout welcomLL = v.findViewById(resID);
+                Animation a = AnimationUtils.loadAnimation(mContext, R.anim.introduction);
+                Animation b = AnimationUtils.loadAnimation(mContext, R.anim.introduction);
+                LinearLayout welcomLL = v.findViewById(R.id.welcome);
                 if(welcomLL.getParent() != null) {
                     ((ViewGroup)welcomLL.getParent()).removeView(welcomLL); // <- fix
                 }
+                TextView welcome = (TextView) welcomLL.findViewById(R.id.welcome_text);
+                a.setStartOffset(1000);welcome.startAnimation(a);
+                TextView brief = (TextView) welcomLL.findViewById(R.id.intro);
+                b.setStartOffset(3000);brief.startAnimation(b);
                 container.addView(welcomLL);
                 return welcomLL;
             case 1:
-                resID = R.id.questions;
-                LinearLayout questionsLL = v.findViewById(resID);
+                LinearLayout questionsLL = v.findViewById(R.id.questions);
                 if(questionsLL.getParent() != null) {
                     ((ViewGroup)questionsLL.getParent()).removeView(questionsLL); // <- fix
                 }
+                setUserName(questionsLL);
                 container.addView(questionsLL);
                 return questionsLL;
             case 2:
-                resID = R.id.event_preferences;
-                LinearLayout eventsLL = v.findViewById(resID);
+                LinearLayout eventsLL = v.findViewById(R.id.event_preferences);
                 if(eventsLL.getParent() != null) {
                     ((ViewGroup)eventsLL.getParent()).removeView(eventsLL); // <- fix
                 }
+                setRadioButtons(eventsLL);
                 container.addView(eventsLL);
                 return eventsLL;
 
             case 3:
-                resID = R.id.conclusion;
-                LinearLayout conclusionLL = v.findViewById(resID);
+                LinearLayout conclusionLL = v.findViewById(R.id.conclusion);
                 if(conclusionLL.getParent() != null) {
                     ((ViewGroup)conclusionLL.getParent()).removeView(conclusionLL); // <- fix
                 }
+                setContinuebutton(conclusionLL);
                 container.addView(conclusionLL);
                 return conclusionLL;
         }
         return v;
+    }
+
+    private void setUserName(View v) {
+        mUsername = (EditText) v.findViewById(R.id.user_name);
+        mUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void setContinuebutton(View v) {
+        allEvents.add("Student Life");
+        allEvents.add("Work");
+        allEvents.add("Festivals");
+        allEvents.add("Home");
+        allEvents.add("Birthdays");
+        allEvents.add("Hangouts");
+        Button mContinue = (Button)v.findViewById(R.id.continue_button);
+        mContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mUserYesStudent.isChecked())
+                    allEvents.remove("Student Life");
+                if (!mUserYesWorked.isChecked())
+                    allEvents.remove("Work");
+                if (!mUserYesReligion.isChecked())
+                    allEvents.remove("Festivals");
+
+                String[] applicableEvents = {};
+                applicableEvents = allEvents.toArray(applicableEvents);
+                StringBuilder combinedEvents = new StringBuilder();
+                for (int i = 0; i < applicableEvents.length; i++)
+                    combinedEvents.append(applicableEvents[i]).append(",");
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(mContext).edit();
+                editor.putBoolean(FIRST_TIME,true);
+                editor.putString(SEND_USERNAME, mUsername.getText().toString());
+                editor.putString(APPLICABLE_EVENTS, combinedEvents.toString());
+                editor.apply();
+                Intent intent = MemoryListActivity.newIntent(mContext);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    private void setRadioButtons(View v) {
+        mUserYesStudent = (RadioButton) v.findViewById(R.id.yes_student);
+        mUserYesWorked = (RadioButton) v.findViewById(R.id.yes_worked);
+        mUserYesReligion = (RadioButton) v.findViewById(R.id.yes_religious);
     }
 
     @Override
--- app/src/main/java/com/example/nostalgia/Introduction.java
+++ app/src/main/java/com/example/nostalgia/Introduction.java
@@ -30,14 +30,9 @@ import java.util.List;
 
 public class Introduction extends AppCompatActivity {
 
-    private EditText mUsername;
-    private Button mContinue;
     public static final String FIRST_TIME = "firsttime";
     public static final String SEND_USERNAME= "username";
     public static final String APPLICABLE_EVENTS = "true_events";
-    public static String globalPreference = "com.example.nostalgia";
-    private RadioButton mUserYesStudent, mUserYesWorked,mUserYesReligious;
-    private List<String> allEvents = new LinkedList<String>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,113 +43,10 @@ public class Introduction extends AppCompatActivity {
             Intent intent = MemoryListActivity.newIntent(getApplicationContext());
             startActivity(intent);
         } else {
-            IntroPagerAdapter introPagerAdapter = new IntroPagerAdapter(getApplicationContext(),getParent());
+            IntroPagerAdapter introPagerAdapter = new IntroPagerAdapter(getApplicationContext());
             ViewPager pager = findViewById(R.id.pager);
             pager.setAdapter(introPagerAdapter);
         }
 
     }
-   /* private void setContinue(Animation a,int delay) {
-        mContinue = (Button) findViewById(R.id.continue_button);
-        a.setStartOffset(delay*1000);mContinue.setAnimation(a);
-        mContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mUserYesStudent.isChecked())
-                    allEvents.remove("Student Life");
-                if (!mUserYesWorked.isChecked())
-                    allEvents.remove("Work");
-                if (!mUserYesReligious.isChecked())
-                    allEvents.remove("Festivals");
-
-                String[] applicableEvents = {};
-                applicableEvents = allEvents.toArray(applicableEvents);
-                StringBuilder combinedEvents = new StringBuilder();
-                for (int i = 0; i < applicableEvents.length; i++)
-                    combinedEvents.append(applicableEvents[i]).append(",");
-                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-                editor.putBoolean(FIRST_TIME,true);
-                editor.putString(SEND_USERNAME, mUsername.getText().toString());
-                editor.putString(APPLICABLE_EVENTS, combinedEvents.toString());
-                editor.apply();
-                Intent intent = MemoryListActivity.newIntent(getApplicationContext());
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void setgetUsername(Animation a, int delay) {
-        mUsername = (EditText) findViewById(R.id.user_name);
-        a.setStartOffset(delay*1000);
-        mUsername.startAnimation(a);
-        mUsername.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-        mUsername.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH ||actionId == EditorInfo.IME_ACTION_DONE || event != null &&
-                                event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    if (event == null || !event.isShiftPressed()) {
-                        int[] introButtonViews = {R.id.student_text,R.id.user_student,R.id.work_text,R.id.user_working
-                                ,R.id.religion_text,R.id.user_religious};
-                        int delay = 1;Animation a = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.introduction);
-                        RadioGroup muserstudent, muserwork, muserreligion;
-                        muserstudent = (RadioGroup) findViewById(R.id.user_student);
-                        muserwork = (RadioGroup) findViewById(R.id.user_working);
-                        muserreligion = (RadioGroup) findViewById(R.id.user_religious);
-                        for (int viewID:introButtonViews){
-                            switch (viewID){
-                                case R.id.student_text:
-                                    setText(a, R.id.student_text, delay);
-                                    break;
-                                case R.id.user_student:
-                                    delay = delay + 5;
-                                    a.setStartOffset(delay * 1000);
-                                    muserstudent.startAnimation(a);
-                                    mUserYesStudent = (RadioButton) findViewById(R.id.yes_student);
-                                    break;
-                                case R.id.work_text:
-                                    setText(a, R.id.work_text, delay);
-                                    break;
-                                case R.id.user_working:
-                                    delay = delay + 5;
-                                    a.setStartOffset(delay * 1000);muserwork.startAnimation(a);
-                                    mUserYesWorked = (RadioButton) findViewById(R.id.yes_worked);
-                                    break;
-                                case R.id.religion_text:
-                                    delay = delay + 5;
-                                    setText(a, R.id.religion_text, delay);
-                                    break;
-                                case R.id.user_religious:
-                                    delay = delay + 5;
-                                    a.setStartOffset(delay * 1000);muserreligion.startAnimation(a);
-                                    mUserYesReligious = (RadioButton) findViewById(R.id.yes_religious);
-                                    break;
-                            }
-                            delay++;
-                        }
-                        if(muserstudent.getCheckedRadioButtonId()!=-1 && muserwork.getCheckedRadioButtonId()!=-1
-                        && muserreligion.getCheckedRadioButtonId()!=-1){
-                            Animation ab = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.introduction);
-                            delay = 1;
-                            setText(a,R.id.thankyou_text,delay);
-                            delay++;
-                            setContinue(a,delay);
-                        }
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
-    }*/
 }
