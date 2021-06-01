5100a12cd3e17895e84121d90d9e3da89d840fb6
Aravind Varma <dl.aravind@gmail.com>
2021-06-01 15:04:55 +0200

    Created new drawerlayout for tablets. Working fine

--- app/src/main/java/com/example/nostalgia/MyGalleryAdapter.java
+++ app/src/main/java/com/example/nostalgia/MyGalleryAdapter.java
@@ -131,11 +131,11 @@ public class MyGalleryAdapter extends RecyclerView.Adapter {
         return videos;
     }          
                
-    private static boolean isImageFile(String path) {
+    public static boolean isImageFile(String path) {
         String mimeType = getMimeType(path);
         return mimeType != null && mimeType.startsWith("image");
     }
-    private static boolean isVideoFile(String path) {
+    public static boolean isVideoFile(String path) {
         String mimeType = getMimeType(path);
         return mimeType != null && mimeType.startsWith("video");
     }
