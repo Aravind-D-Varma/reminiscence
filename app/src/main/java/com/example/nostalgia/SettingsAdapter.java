7f8029f516ce4dfb751e36350d6a8cc53afadedd
Aravind Varma <dl.aravind@gmail.com>
2021-06-04 13:37:45 +0200

    Rewrote UserSettingsActivity. Deleted past code

--- app/src/main/java/com/example/nostalgia/SettingsAdapter.java
+++ app/src/main/java/com/example/nostalgia/SettingsAdapter.java
@@ -1,8 +1,14 @@
 package com.example.nostalgia;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class SettingsAdapter extends BaseExpandableListAdapter {
 
@@ -15,14 +21,20 @@ public class SettingsAdapter extends BaseExpandableListAdapter {
     private static final int USERNAME_GROUP = 0;
     private static final int EVENTS_GROUP = 1;
     private static final int  ABOUT_GROUP = 2;
+    private final Context mContext;
+
+    public SettingsAdapter(Context context){
+        this.mContext = context;
+    }
+
     @Override
     public int getGroupCount() {
-        return 0;
+        return 3;
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return 4;
     }
 
     @Override
@@ -37,27 +49,126 @@ public class SettingsAdapter extends BaseExpandableListAdapter {
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return childPosition;
     }
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        Integer groupType = getGroupType(groupPosition);
+
+        if (convertView == null || convertView.getTag() != groupType) {
+            switch (groupType) {
+                case USERNAME_GROUP :
+                    convertView = inflater.inflate(R.layout.settings_username, null);
+                    break;
+                case EVENTS_GROUP:
+                    // Am using the same layout cause am lasy and don't wanna create other ones but theses should be different
+                    // or the group type shouldnt exist
+                    convertView = inflater.inflate(R.layout.settings_event, null);
+                    break;
+                case ABOUT_GROUP:
+                    // Am using the same layout cause am lasy and don't wanna create other ones but theses should be different
+                    // or the group type shouldnt exist
+                    convertView = inflater.inflate(R.layout.settings_myself, null);
+                    break;
+                default:
+                    // Maybe we should implement a default behaviour but it should be ok we know there are 3 group types right?
+                    break;
+            }
+        }
+        // We'll reuse the existing one
+        else {
+            // There is nothing to do here really we just need to set the content of view which we do in both cases
+        }
+
+        switch (groupType) {
+            case USERNAME_GROUP :
+                TextView item = (TextView) convertView.findViewById(R.id.textView);
+                item.setText("Texview of USERNAME_GROUP in getGroupView");
+                break;
+            case EVENTS_GROUP:
+                TextView item2 = (TextView) convertView.findViewById(R.id.textView);
+                item2.setText("texview of EVENTS_GROUP in getGroupView");
+                break;
+            case ABOUT_GROUP:
+                TextView item3 = (TextView) convertView.findViewById(R.id.textView);
+                item3.setText("texview of ABOUT_GROUP in getGroupView");
+                break;
+            default:
+                // Maybe we should implement a default behaviour but it should be ok we know there are 3 group types right?
+                break;
+        }
+
+        return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        Integer childType = getChildType(groupPosition, childPosition);
+
+        // We need to create a new "cell container"
+        if (convertView == null || convertView.getTag() != childType) {
+            switch (childType) {
+                case TEXT_CHILD_TYPE_1:
+                    convertView = inflater.inflate(R.layout.settings_username, null);
+                    convertView.setTag(childType);
+                    break;
+                case EDITTEXT_CHILD_TYPE_2:
+                    convertView = inflater.inflate(R.layout.settings_myself, null);
+                    convertView.setTag(childType);
+                    break;
+                case RV_CHILD_TYPE_3:
+                    convertView = inflater.inflate(R.layout.settings_myself, null);
+                    convertView.setTag(childType);
+                    break;
+                case CHILD_TYPE_UNDEFINED:
+                    convertView = inflater.inflate(R.layout.settings_myself, null);
+                    convertView.setTag(childType);
+                    break;
+                default:
+                    // Maybe we should implement a default behaviour but it should be ok we know there are 4 child types right?
+                    break;
+            }
+        }
+        // We'll reuse the existing one
+        else {
+            // There is nothing to do here really we just need to set the content of view which we do in both cases
+        }
+
+        switch (childType) {
+            case TEXT_CHILD_TYPE_1:
+                TextView item = (TextView) convertView.findViewById(R.id.textView);
+                item.setText("texview of USERNAME_GROUP in getChildView");
+                break;
+            case EDITTEXT_CHILD_TYPE_2:
+                //Define how to render the data on the CHILD_TYPE_2 layout
+                TextView item2 = (TextView) convertView.findViewById(R.id.textView);
+                item2.setText("texview of EVENTS_GROUP in getChildView");
+                break;
+            case RV_CHILD_TYPE_3:
+                //Define how to render the data on the CHILD_TYPE_3 layout
+                TextView item3 = (TextView) convertView.findViewById(R.id.textView);
+                item3.setText("texview of ABOUT_GROUP in getGroupView");
+                break;
+            case CHILD_TYPE_UNDEFINED:
+                //Define how to render the data on the CHILD_TYPE_UNDEFINED layout
+                break;
+        }
+
+        return convertView;
     }
 
     @Override
@@ -67,12 +178,51 @@ public class SettingsAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getChildType(int groupPosition, int childPosition) {
-        return super.getChildType(groupPosition, childPosition);
+        switch (groupPosition) {
+            case USERNAME_GROUP:
+                switch (childPosition) {
+                    case 0:
+                        return TEXT_CHILD_TYPE_1;
+                    case 1:
+                        return EDITTEXT_CHILD_TYPE_2;
+                    case 2:
+                        return CHILD_TYPE_UNDEFINED;
+                }
+                break;
+            case EVENTS_GROUP:
+                switch (childPosition) {
+                    case 0:
+                        return TEXT_CHILD_TYPE_1;
+                    case 1:
+                        return RV_CHILD_TYPE_3;
+                    case 2:
+                        return CHILD_TYPE_UNDEFINED;
+                }
+                break;
+            case ABOUT_GROUP:
+                switch (childPosition) {
+                    case 0:
+                        return TEXT_CHILD_TYPE_1;
+                    default:
+                        return CHILD_TYPE_UNDEFINED;
+                }
+            default:
+                return CHILD_TYPE_UNDEFINED;
+        }
+        return CHILD_TYPE_UNDEFINED;
     }
 
     @Override
     public int getGroupType(int groupPosition) {
-        return super.getGroupType(groupPosition);
+        switch (groupPosition) {
+            case 0:
+                return USERNAME_GROUP;
+            case 1:
+                return EVENTS_GROUP;
+            case 2:
+                return ABOUT_GROUP;
+        }
+        return ABOUT_GROUP;
     }
 
     @Override
--- app/src/main/java/com/example/nostalgia/UserSettingsActivity.java
+++ app/src/main/java/com/example/nostalgia/UserSettingsActivity.java
@@ -1,17 +1,14 @@
 package com.example.nostalgia;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.nostalgia.SettingsAdapter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,89 +18,16 @@ import java.util.List;
 public class UserSettingsActivity extends AppCompatActivity {
     private EditText mUsername;
     private ListView mListView;
-    private List<String> allEvents = new LinkedList<String>();
+    private SettingsAdapter mSettingsAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle(getString(R.string.settings));
-        setContentView(R.layout.all_settings);
+        setContentView(R.layout.settings_all);
         mListView = (ExpandableListView) findViewById(R.id.expandable_listview);
-        SettingsAdapter settingsAdapter = new SettingsAdapter();
-        gettingUserPreferences();
-    }
-
-    private void gettingUserPreferences() {
-        allEvents = initializeEvents(allEvents);
-        setUserName();
-    }
-
-    private void setUserName() {
-        mUsername = (EditText) findViewById(R.id.user_name);
-        mUsername.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-    }
-
-
-    private List<String> initializeEvents(List<String> allEvents) {
-        allEvents.add("Student Life");
-        allEvents.add("Work");
-        allEvents.add("Festivals");
-        allEvents.add("Home");
-        allEvents.add("Birthdays");
-        allEvents.add("Hangouts");
-        return allEvents;
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        saveUserPreferences();
-    }
-
-    private void saveUserPreferences() {
-        if (noActivityInSettings()) {
-            Toast.makeText(getApplicationContext(), "You didnt make any changes. Keeping your previous settings...", Toast.LENGTH_SHORT).show();
-        }
-        else if(onlyChangedName()) {
-            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-            editor.putString(Introduction.SEND_USERNAME, mUsername.getText().toString());
-            editor.apply();
-        }
-        else{
-                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-
-                editor.putString(Introduction.APPLICABLE_EVENTS, listOfStringsToString(allEvents));
-                editor.apply();
-        }
-    }
-
-    private String listOfStringsToString(List<String> allEvents) {
-        String[] applicableEvents = {};
-        applicableEvents = allEvents.toArray(applicableEvents);
-        StringBuilder combinedEvents = new StringBuilder();
-        for (int i = 0; i < applicableEvents.length; i++)
-            combinedEvents.append(applicableEvents[i]).append(",");
-        return combinedEvents.toString();
-    }
-
-    private boolean onlyChangedName() {
-        return true;
-    }
-
-    private boolean noActivityInSettings() {
-        return mUsername.getText().toString().length() < 1;
+        mSettingsAdapter = new SettingsAdapter(this);
+        mListView.setAdapter((ListAdapter) mSettingsAdapter);
     }
 
 }
