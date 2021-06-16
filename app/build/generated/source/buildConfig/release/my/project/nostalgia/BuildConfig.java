05e23b5cae6530146d6b441725017e1d1781f546
Aravind Varma <dl.aravind@gmail.com>
2021-06-16 15:31:01 +0200

    Pre-Release apk and androitTest starting

--- app/build/generated/source/buildConfig/release/my/project/nostalgia/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/my/project/nostalgia/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "my.project.nostalgia";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "Alpha";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "Pre-Release";
 }
