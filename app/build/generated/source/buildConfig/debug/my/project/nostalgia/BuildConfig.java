b26a10bb6422a39a6794a370d6b9be8ca090e7c3
Aravind Varma <dl.aravind@gmail.com>
2021-06-18 11:41:41 +0200

    Changed introduction text

--- app/build/generated/source/buildConfig/debug/my/project/nostalgia/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/my/project/nostalgia/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "my.project.nostalgia";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "Alpha";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "Pre-Release";
 }
