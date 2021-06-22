687fb1a0001243e17db1e72d54b83a88bd1c0eae
Aravind Varma <dl.aravind@gmail.com>
2021-06-22 11:20:35 +0200

    Changed introduction pages. Only two pages and added sign-in button

--- app/src/main/java/my/project/nostalgia/adapters/IntroductionPagerAdapter.java
+++ app/src/main/java/my/project/nostalgia/adapters/IntroductionPagerAdapter.java
@@ -1,5 +1,4 @@
 package my.project.nostalgia.adapters;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,7 +6,6 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -15,9 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
 
-import my.project.nostalgia.activities.MemoryListActivity;
 import my.project.nostalgia.R;
-import my.project.nostalgia.supplementary.memoryEvents;
 
 import java.util.UUID;
 
@@ -49,58 +45,46 @@ public class IntroductionPagerAdapter extends PagerAdapter {
 
         switch(position){
             case 0:
-                LinearLayout welcomeLL = getLayoutAndFix(v, R.id.welcome);
+                LinearLayout welcomeLL = v.findViewById(R.id.welcome);
+                if (welcomeLL.getParent() != null) {
+                    ((ViewGroup) welcomeLL.getParent()).removeView(welcomeLL); // <- fix
+                }
                 container.addView(welcomeLL);
                 return welcomeLL;
             case 1:
-                LinearLayout questionsLL = getLayoutAndFix(v, R.id.questions);
-                mUsername = (EditText) questionsLL.findViewById(R.id.user_name);
-                container.addView(questionsLL);
-                return questionsLL;
-            case 2:
                 ConstraintLayout conclusionLL = v.findViewById(R.id.conclusion);
                 if (conclusionLL.getParent() != null) {
                     ((ViewGroup) conclusionLL.getParent()).removeView(conclusionLL); // <- fix
                 }
-                setContinuebutton(conclusionLL);
+
+                v.findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                    }
+                });
                 container.addView(conclusionLL);
                 return conclusionLL;
         }
         return v;
     }
-
-    /**
-     * Initialises layout depending on where the user is in viewPager.
-     * Fixes IllegalStateException: the child already has parent
-     */
-    private LinearLayout getLayoutAndFix(View v, int LinearLayoutID) {
-        LinearLayout welcomLL = v.findViewById(LinearLayoutID);
-        if (welcomLL.getParent() != null) {
-            ((ViewGroup) welcomLL.getParent()).removeView(welcomLL); // <- fix
-        }
-        return welcomLL;
-    }
-    /**
-     * Three layouts: welcome, username and continue
-     * @return
-     */
+    /**Two layouts: welcome and sign in*/
     @Override
     public int getCount() {
-        return 3;
+        return 2;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
     }
-    /**
-     * important to remove the super.destoryItem method
-     */
+    /**important to remove the super.destoryItem method*/
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
     }
 
-    private void setContinuebutton(View v) {
+    /*private void setContinuebutton(View v) {
         Button mContinue = (Button)v.findViewById(R.id.continue_button);
         mContinue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,7 +100,7 @@ public class IntroductionPagerAdapter extends PagerAdapter {
                 ((Activity)mContext).finish();
             }
         });
-    }
+    }*/
     /**
      * Sets up the username and applicable events for access across application by using SharedPreferences.
      * @param userName
